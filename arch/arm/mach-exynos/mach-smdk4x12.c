9c3ca734247c922c14ea91b3e3eb66ae1f24f294
abc <abc@abc-virtual-machine.(none)>
2015-03-25 08:52:23 +0800

    add cyttsp5 touchscreen
    	modified:   arch/arm/mach-exynos/mach-smdk4x12.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_btn.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_core.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_debug.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_device_access.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_devtree.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_i2c.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_loader.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_mt_common.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_mta.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_mtb.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_platform.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_proximity.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_regs.h
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_spi.c
    	new file:   drivers/input/touchscreen/cyttsp5/cyttsp5_test_device_access_api.c
    	new file:   include/linux/cyttsp5_core.h
    	new file:   include/linux/cyttsp5_device_access-api.h
    	new file:   include/linux/cyttsp5_platform.h

--- arch/arm/mach-exynos/mach-smdk4x12.c
+++ arch/arm/mach-exynos/mach-smdk4x12.c
@@ -157,6 +157,22 @@ EXPORT_SYMBOL(g_ddr_vol);
 				 S5PV210_UFCON_TXTRIG4 |	\
 				 S5PV210_UFCON_RXTRIG4)
 
+/* cyttsp */
+#include <linux/cyttsp5_core.h>
+#include <linux/cyttsp5_platform.h>
+
+#ifdef CONFIG_TOUCHSCREEN_CYPRESS_CYTTSP5_I2C
+#define CYTTSP5_USE_I2C 1
+
+#ifdef CYTTSP5_USE_I2C
+#define CYTTSP5_I2C_TCH_ADR 0x24
+#define CYTTSP5_LDR_TCH_ADR 0x24
+#define CYTTSP5_I2C_IRQ_GPIO		EXYNOS4_GPX0(4) /* J6.9, C19, GPMC_AD14/GPIO_38 */
+#define CYTTSP5_I2C_IRQ_GPIO_TO_RIQ	IRQ_EINT(4) 
+#define CYTTSP5_I2C_RST_GPIO	EXYNOS4212_GPM3(4) /* 127 J6.10, D18, GPMC_AD13/GPIO_37 */
+#endif
+#endif // CONFIG_TOUCHSCREEN_CYPRESS_CYTTSP5_I2C
+
 static struct s3c2410_uartcfg smdk4x12_uartcfgs[] __initdata = {
 	[0] = {
 		.hwport		= 0,
@@ -2999,6 +3015,135 @@ struct MAX8647_platform_data max8647_data = {
 
 #endif
 
+#ifdef CYTTSP5_USE_I2C	
+
+#define CYTTSP5_HID_DESC_REGISTER 1
+
+	#define CY_VKEYS_X 720
+	#define CY_VKEYS_Y 1280
+	#define CY_MAXX 880
+	#define CY_MAXY 1280
+	#define CY_MINX 0
+	#define CY_MINY 0
+
+	#define CY_ABS_MIN_X CY_MINX
+	#define CY_ABS_MIN_Y CY_MINY
+	#define CY_ABS_MAX_X CY_MAXX
+	#define CY_ABS_MAX_Y CY_MAXY
+	#define CY_ABS_MIN_P 0
+	#define CY_ABS_MAX_P 255
+	#define CY_ABS_MIN_W 0
+	#define CY_ABS_MAX_W 255
+	#define CY_PROXIMITY_MIN_VAL	0
+	#define CY_PROXIMITY_MAX_VAL	1
+
+	#define CY_ABS_MIN_T 0
+
+	#define CY_ABS_MAX_T 15
+
+static u16 cyttsp5_btn_keys[] = {
+	/* use this table to map buttons to keycodes (see input.h) */
+	KEY_HOMEPAGE,		/* 172 */ /* Previously was KEY_HOME (102) */
+				/* New Android versions use KEY_HOMEPAGE */
+	KEY_MENU,		/* 139 */
+	KEY_BACK,		/* 158 */
+	KEY_SEARCH,		/* 217 */
+	KEY_VOLUMEDOWN,		/* 114 */
+	KEY_VOLUMEUP,		/* 115 */
+	KEY_CAMERA,		/* 212 */
+	KEY_POWER		/* 116 */
+};
+
+static struct touch_settings cyttsp5_sett_btn_keys = {
+	.data = (uint8_t *)&cyttsp5_btn_keys[0],
+	.size = ARRAY_SIZE(cyttsp5_btn_keys),
+	.tag = 0,
+};
+
+static struct cyttsp5_core_platform_data _cyttsp5_core_platform_data = {
+	.irq_gpio = CYTTSP5_I2C_IRQ_GPIO,
+	.rst_gpio = CYTTSP5_I2C_RST_GPIO,
+	.hid_desc_register = CYTTSP5_HID_DESC_REGISTER,
+	.xres = cyttsp5_xres,
+	.init = cyttsp5_init,
+	.power = cyttsp5_power,
+	.detect = cyttsp5_detect,
+	.irq_stat = cyttsp5_irq_stat,
+	.sett = {
+		NULL,	/* Reserved */
+		NULL,	/* Command Registers */
+		NULL,	/* Touch Report */
+		NULL,	/* Cypress Data Record */
+		NULL,	/* Test Record */
+		NULL,	/* Panel Configuration Record */
+		NULL,	/* &cyttsp5_sett_param_regs, */
+		NULL,	/* &cyttsp5_sett_param_size, */
+		NULL,	/* Reserved */
+		NULL,	/* Reserved */
+		NULL,	/* Operational Configuration Record */
+		NULL, /* &cyttsp5_sett_ddata, *//* Design Data Record */
+		NULL, /* &cyttsp5_sett_mdata, *//* Manufacturing Data Record */
+		NULL,	/* Config and Test Registers */
+		&cyttsp5_sett_btn_keys,	/* button-to-keycode table */
+	},
+	.flags = CY_CORE_FLAG_WAKE_ON_GESTURE
+			| CY_CORE_FLAG_RESTORE_PARAMETERS,
+	.easy_wakeup_gesture = CY_CORE_EWG_NONE,
+};
+
+static const int16_t cyttsp5_abs[] = {
+	ABS_MT_POSITION_X, CY_ABS_MIN_X, CY_ABS_MAX_X, 0, 0,
+	ABS_MT_POSITION_Y, CY_ABS_MIN_Y, CY_ABS_MAX_Y, 0, 0,
+	ABS_MT_PRESSURE, CY_ABS_MIN_P, CY_ABS_MAX_P, 0, 0,
+	CY_IGNORE_VALUE, CY_ABS_MIN_W, CY_ABS_MAX_W, 0, 0,
+	ABS_MT_TRACKING_ID, CY_ABS_MIN_T, CY_ABS_MAX_T, 0, 0,
+	ABS_MT_TOUCH_MAJOR, 0, 255, 0, 0,
+	ABS_MT_TOUCH_MINOR, 0, 255, 0, 0,
+	ABS_MT_ORIENTATION, -127, 127, 0, 0,
+	ABS_MT_TOOL_TYPE, 0, MT_TOOL_MAX, 0, 0,
+	ABS_DISTANCE, 0, 255, 0, 0,	/* Used with hover */
+};
+
+struct touch_framework cyttsp5_framework = {
+	.abs = (uint16_t *)&cyttsp5_abs[0],
+	.size = ARRAY_SIZE(cyttsp5_abs),
+	.enable_vkeys = 0,
+};
+static struct cyttsp5_mt_platform_data _cyttsp5_mt_platform_data = {
+	.frmwrk = &cyttsp5_framework,
+	.flags = CY_MT_FLAG_INV_X | CY_MT_FLAG_INV_Y,
+	.inp_dev_name = CYTTSP5_MT_NAME,
+	.vkeys_x = CY_VKEYS_X,
+	.vkeys_y = CY_VKEYS_Y,
+};
+
+static struct cyttsp5_btn_platform_data _cyttsp5_btn_platform_data = {
+	.inp_dev_name = CYTTSP5_BTN_NAME,
+};
+
+static const int16_t cyttsp5_prox_abs[] = {
+	ABS_DISTANCE, CY_PROXIMITY_MIN_VAL, CY_PROXIMITY_MAX_VAL, 0, 0,
+};
+
+struct touch_framework cyttsp5_prox_framework = {
+	.abs = (uint16_t *)&cyttsp5_prox_abs[0],
+	.size = ARRAY_SIZE(cyttsp5_prox_abs),
+};
+
+static struct cyttsp5_proximity_platform_data
+		_cyttsp5_proximity_platform_data = {
+	.frmwrk = &cyttsp5_prox_framework,
+	.inp_dev_name = CYTTSP5_PROXIMITY_NAME,
+};
+
+static struct cyttsp5_platform_data _cyttsp5_platform_data = {
+	.core_pdata = &_cyttsp5_core_platform_data,
+	.mt_pdata = &_cyttsp5_mt_platform_data,
+	.loader_pdata = &_cyttsp5_loader_platform_data,
+	.btn_pdata = &_cyttsp5_btn_platform_data,
+	.prox_pdata = &_cyttsp5_proximity_platform_data,
+};
+#endif // CYTTSP5_USE_I2C	
 //*********************************************
 
 static struct i2c_board_info i2c_devs0[] __initdata = {
@@ -3051,8 +3196,15 @@ static struct i2c_board_info i2c_devs2[] __initdata = {
 };
 
 static struct i2c_board_info i2c_devs3[] __initdata = {
-	
+#ifdef CYTTSP5_USE_I2C	
+	{
+		I2C_BOARD_INFO(CYTTSP5_I2C_NAME, CYTTSP5_I2C_TCH_ADR),
+		.irq =  CYTTSP5_I2C_IRQ_GPIO_TO_RIQ,
+		.platform_data = &_cyttsp5_platform_data,
+	},
+#else
 	{	I2C_BOARD_INFO("ft5x0x_ts",0x38),   },
+#endif
 };
 static struct i2c_board_info i2c_devs4[] __initdata = {
 	{
